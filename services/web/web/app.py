c506ffbc4976577342bf20cb6d2f57d3751eb4db
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2021-02-24 06:40:53 +0900

    use session (hatena_id)

--- services/web/web/app.py
+++ services/web/web/app.py
@@ -1,46 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, session, url_for
+import os
 
 from learner import learner
 from manager import manager
 
 app = Flask(__name__, template_folder='/work/templates')
 
-hatena_id = ""
+app.secret_key = os.urandom(24)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    image = None
+    hatena_id = ""
     entries = []
+    image = None
     if request.method == 'GET':
-        if hatena_id:
-            u = manager.create_user(hatena_id)
+        if 'hatena_id' in session:
+            hatena_id = session['hatena_id']
+            u = manager.create_user(session['hatena_id'])
             if u.wordcloud:
                 image = u.wordcloud.decode("utf-8")
-            entries = sorted(learner.get_suggestion(hatena_id),
+            entries = sorted(learner.get_suggestion(session['hatena_id']),
                         key=lambda x: x.score, reverse=True)
-    return render_template("index.html", hatena_id=hatena_id, entries=entries, image=image)
+        return render_template("index.html", hatena_id=hatena_id, entries=entries, image=image)
 
 @app.route('/id', methods=['POST'])
 def id():
     if request.method == 'POST':
-        global hatena_id
         id = request.form["user_id"]
         if learner.exists_hatena_id(id):
-            hatena_id = id
-            u = manager.create_user(hatena_id)
+            session['hatena_id'] = id
+            u = manager.create_user(id)
     return redirect(url_for('index'))
 
 @app.route('/wordcloud', methods=['POST'])
 def wordcloud():
     if request.method == 'POST':
-        if hatena_id:
-            wordcloud = learner.create_wordcloud(hatena_id)
+        if 'hatena_id' in session:
+            wordcloud = learner.create_wordcloud(session['hatena_id'])
     return redirect(url_for('index'))
 
 @app.route('/learn', methods=['POST'])
 def learn():
     if request.method == 'POST':
-        learner.learn(hatena_id)
+        learner.learn(session['hatena_id'])
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
