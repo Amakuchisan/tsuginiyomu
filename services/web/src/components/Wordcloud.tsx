046c6c89d827fefe0eb79b8d0f7e52c0744e0dfb
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2021-03-03 09:16:38 +0000

    i didnt use useState

--- services/web/src/components/Wordcloud.tsx
+++ services/web/src/components/Wordcloud.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 
 import { CreateWordCloudRequest } from "../pb/learner/learner_pb";
 import { LearnerClient } from "../pb/learner/LearnerServiceClientPb";
