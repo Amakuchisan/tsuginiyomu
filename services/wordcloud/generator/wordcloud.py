833a68bd86d89b046df908840d3440220c68a86f
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2021-03-04 10:08:44 +0000

    wordcloudを作成する単語データをDBから取得

--- services/wordcloud/generator/wordcloud.py
+++ services/wordcloud/generator/wordcloud.py
@@ -1,13 +1,26 @@
+from base64 import b64encode
+import io
 import os
 from wordcloud import WordCloud
+from manager import user, word
 
-#ワードクラウドの作成
-def create_wordcloud(text: str):
-    if len(text) == 0:
-        text = "あなたの ワードクラウドを 作成しよう"
-        word_chain = text
-    else:
-        word_chain = ' '.join(text)
+# ワードクラウドの更新
+def update_wordcloud(hatena_id: str) -> bytes:
+    frequencies = {}
+    if hatena_id != "":
+        frequencies = word.find_word(hatena_id)
+
+    output = io.BytesIO()
+    create_wordcloud(frequencies).save(output, format='PNG')
+    wordcloud_image = b64encode(output.getvalue())
+    user.update_wordcloud(hatena_id, wordcloud_image)
+
+    return wordcloud_image
+
+# ワードクラウドの作成
+def create_wordcloud(frequencies: dict[str, int]) : #  -> PIL.Image.Image
+    if len(frequencies) == 0:
+        frequencies = {"データが": 1, "学習されている": 2, "ありません": 1}
 
     fontpath = '/work/.fonts/' + os.environ['FONTFILE']
 
@@ -17,9 +30,8 @@ def create_wordcloud(text: str):
                           width=900,
                           height=500,
                           relative_scaling=0.5 # フォントサイズの相対的な単語頻度の重要性
-                         ).generate(word_chain)
+                         ).fit_words(frequencies)
 
     #ファイルの作成
     img = wordcloud.to_image()
-    # img.save("/work/images/image-" + os.environ['HATENAID'] + ".png", optimize=True)
     return img
--- services/wordcloud/server/wordcloud.py
+++ services/wordcloud/server/wordcloud.py
@@ -1,11 +1,10 @@
 import grpc
 
-from bookmark.bookmark import Bookmark
+from generator import wordcloud
 import pb.wordcloud.wordcloud_pb2 as wordcloud_pb2
 import pb.wordcloud.wordcloud_pb2_grpc as wordcloud_pb2_grpc
 
 class Wordcloud(wordcloud_pb2_grpc.WordcloudServicer):
     def CreateWordCloud(self, request, context):
-        bookmark = Bookmark()
-        wordcloud = bookmark.update_wordcloud(request.hatena_id)
-        return wordcloud_pb2.CreateWordCloudReply(wordcloud=wordcloud)
+        wordcloud_image = wordcloud.update_wordcloud(request.hatena_id)
+        return wordcloud_pb2.CreateWordCloudReply(wordcloud=wordcloud_image)
